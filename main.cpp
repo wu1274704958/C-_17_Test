cf868f66f036d2203532313ab4d39de5a029f7cc
wws <1274704958@qq.com>
2018-03-03 18:34:37 +0800

    add main.cpp

--- main.cpp
+++ main.cpp
@@ -3,18 +3,33 @@
 #include <string_view>
 using namespace std;
 
-template<typename T,int ...I>
-void print_tuple(T tu,index_sequence<I...> is)
-{
-	((cout << get<I>(tu) << " "), ...);
-	cout << "\n";
+namespace n1 {
+
+	template<typename T, int ...I>
+	void print_tuple(T tu, index_sequence<I...> is)
+	{
+		((cout << get<I>(tu) << " "), ...);
+		cout << "\n";
+	}
+
+	void test()
+	{
+		int a = 0;
+		if (a = pow(2, 2); a > 8)
+		{
+			cout << a << endl;
+		}
+
+		auto tup = make_tuple(2018, 2.25f, string_view("HelloWorld"));
+		print_tuple(tup, make_index_sequence<tuple_size<decltype(tup)>::value>());
+		system("pause");
+	}
 }
+
 
 int main()
 {
-	auto tup = make_tuple(2018, 2.25f, string_view("HelloWorld"));
-	print_tuple(tup, make_index_sequence<tuple_size<decltype(tup)>::value>());
+	n1::test();
 
-	system("pause");
 	return 0;
 }
